1eb18e8d6d585726d0553c2f61537f9f637fad90
yaozhihui <3351561983@qq.com>
2021-11-11 17:07:51 +0800

    class

---
+++ itripbeans/src/main/java/cn/itrip/beans/App.java
@@ -0,0 +1,13 @@
+package cn.itrip.beans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
